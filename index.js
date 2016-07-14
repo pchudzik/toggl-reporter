1ab542e0973589f66d54909d42cceb06a76106ba
Paweł Chudzik <pawel.chudzik@gmail.com>
2016-07-15 01:35:06 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+  if(req.url.startsWith("/api")) {
+    proxy.web(req, res, {
+      secure: false,
+      target: 'https://www.toggl.com'
+    });
+  } else {
+    serve(req, res, finalhandler(req, res));
+  }
+})
+.listen('8080');
---
+++ settings.js
@@ -0,0 +1,2 @@
+$.fn.datepicker.defaults.format = "dd-mm-yyyy";
+$.fn.datepicker.defaults.weekStart = 1;
